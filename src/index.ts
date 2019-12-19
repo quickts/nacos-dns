4bbd9c065aa6803033937dc35f02c4f792c5b18f
chengaoyuan <chen.gaoyuan@qq.com>
2019-12-19 15:28:31 +0800

    no message

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import 'dotenv/config';
 import dnsd from 'modern-dnsd';
 import { configure } from 'log4js';
 import { buildConfig } from './log4js.config';
-import { DnsResolver } from './dns.resolver';
+import { NacosDnsResolver } from './nacos.dns.resolver';
 configure(buildConfig(process.env.LOG_LEVEL || 'INFO'));
 
-const resolver = new DnsResolver();
+const resolver = new NacosDnsResolver();
 const server = dnsd.createServer(resolver.handler());
 server.listen(Number(process.env.DNS_PORT));
